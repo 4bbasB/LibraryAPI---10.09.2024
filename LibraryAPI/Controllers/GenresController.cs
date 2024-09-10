a2116eb5e876d885b04c0b1c1ead3fee4e3ffbd8
4bbasB <abbasbayramli1719@gmail.com>
2024-09-10 11:49:31 +0400

    Initial Commit

---
+++ LibraryAPI/Controllers/GenresController.cs
@@ -0,0 +1,37 @@
+﻿using LibraryAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private List<Genre> _genres = new List<Genre>()
+        {
+            new Genre()
+            {
+                Id = 1,
+                Name = "Dram"
+            },
+            new Genre()
+            {
+                Id = 2,
+                Name = "Sci-Fi"
+            },
+            new Genre()
+            {
+                Id = 3,
+                Name = "Horror"
+            }
+        };
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_genres);
+        }
+    }
+
+}
